8a53a332c3ab8268ae3f9c936c71adce5aabfbcf
Teresa Wu <teresa.wu.jy@gmail.com>
2023-05-15 23:40:40 -0700

    Reinstalled Flowbite-Svelte and tailwindcss

--- task-manager/src/routes/+page.svelte
+++ task-manager/src/routes/+page.svelte
@@ -5,8 +5,23 @@
 	import { Alert } from 'flowbite-svelte';
 </script>
 
+<svelte:head>
+	<title>Task Manager</title>
+	<meta name="description" content="Task manager app in the making" />
+  <style>
+    /* @import url('https://fonts.googleapis.com/css2?family=Pacifico&display=swap'); */
+    /* <link href="https://fonts.googleapis.com/css2?family=Pacifico&display=swap" rel="stylesheet"> */
+  </style>
+</svelte:head>
+
 <div class="p-8">
 	<Alert>
 		<span class="font-medium">Info alert!</span> Change a few things up and try submitting again.
 	</Alert>
 </div>
+
+<section>
+  <div class="container mx-auto max-w-lg h-[600px] p-4 border-solid border-black rounded-lg shadow-lg">
+    <p class="text-2xl font-bold text-center font-pacifico">Heading Test</p>
+  </div>
+</section>
--- task-manager/tailwind.config.cjs
+++ task-manager/tailwind.config.cjs
@@ -1,15 +1,3 @@
-// const config = {
-// 	content: ['./src/**/*.{html,js,svelte,ts}'],
-
-// 	theme: {
-// 		extend: {}
-// 	},
-
-// 	plugins: []
-// };
-
-// module.exports = config;
-
 const config = {
   content: [
     "./src/**/*.{html,js,svelte,ts}",
@@ -24,6 +12,13 @@ const config = {
   
   theme: {
     extend: {
+			
+			screens: {
+				sm: "480px",
+				md: "768px",
+				lg: "967px",
+				xl: "1440px",
+			},
       colors: {
         // flowbite-svelte
          primary: {50:'#ebf5ff',100: '#fff1ee',200:'#ffe4de',300:'#ffd5cc',400:'#ffbcad',500:'#fe795d',600:'#ef562f',700:'#eb4f27',800:'#d3330a',900:'#d3330a'}
