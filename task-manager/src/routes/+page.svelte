bda633543e91b622235d7577139b63af3e522cad
Teresa Wu <teresa.wu.jy@gmail.com>
2023-06-08 22:02:10 -0700

    Added tabs component with varying task priorities

--- task-manager/src/routes/+page.svelte
+++ task-manager/src/routes/+page.svelte
@@ -4,6 +4,7 @@
 <script>
 	import { Alert } from "flowbite-svelte";
   import { Footer, FooterCopyright, FooterLinkGroup, FooterLink } from "flowbite-svelte";
+  import { Tabs, TabItem } from "flowbite-svelte";
 </script>
 
 <svelte:head>
@@ -21,17 +22,51 @@
 	</Alert>
 </div>
 
-
 <section>
-  <div class="container mx-auto max-w-lg h-[600px] p-4 border-solid border-black rounded-lg shadow-lg">
-    <p class="text-2xl font-bold text-center font-pacifico">Heading Test</p>
-  </div>
+
+  <Tabs class="container mx-auto max-w-lg">
+    <TabItem open title="High">
+      <div class="container mx-auto max-w-lg h-[600px] p-4 border-solid border-black rounded-lg shadow-lg">
+        <p class="text-2xl font-bold text-center font-pacifico">High Header</p>
+      </div>
+    </TabItem>
+    <TabItem open title="Medium">
+      <div class="container mx-auto max-w-lg h-[600px] p-4 border-solid border-black rounded-lg shadow-lg">
+        <p class="text-2xl font-bold text-center font-pacifico">Medium Header</p>
+      </div>
+    </TabItem>
+    <TabItem open title="Low">
+      <div class="container mx-auto max-w-lg h-[600px] p-4 border-solid border-black rounded-lg shadow-lg">
+        <p class="text-2xl font-bold text-center font-pacifico">Low Header</p>
+      </div>
+    </TabItem>
+    <TabItem open title="Notes">
+      <div class="container mx-auto max-w-lg h-[600px] p-4 border-solid border-black rounded-lg shadow-lg">
+        <p class="text-2xl font-bold text-center font-pacifico">Notes</p>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quos error vero dolorem explicabo ab, architecto blanditiis optio excepturi mollitia veniam nobis natus! Non a quo iure perspiciatis esse accusantium! Ab?</p>
+      </div>
+    </TabItem>
+  </Tabs>
+
+  <!-- Card example
+  <div class="max-w-sm mx-auto rounded overflow-hidden shadow-lg">
+    <img class="w-full" src="https://v1.tailwindcss.com/img/card-top.jpg" alt="Sunset in the mountains">
+    <div class="px-6 py-4">
+      <div class="font-bold text-xl mb-2">The Coldest Sunset</div>
+      <p class="text-gray-700 text-base">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.
+      </p>
+    </div>
+    <div class="px-6 pt-4 pb-2">
+      <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#photography</span>
+      <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#travel</span>
+      <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#winter</span>
+    </div>
+  </div> -->
 </section>
 
-
-<!-- Temporary footer -->
 <Footer>
-  <FooterCopyright href="/" by="Flowbite™" year={2023} />
+  <FooterCopyright href="/" by="Flowbite™" year={2023} copyrightMessage="This is a test footer and needs to be stickied to the bottom" />
   <FooterLinkGroup ulClass="flex flex-wrap items-center mt-3 text-sm text-gray-500 dark:text-gray-400 sm:mt-0">
     <FooterLink href="/">About</FooterLink>
     <FooterLink href="/">Privacy Policy</FooterLink>
